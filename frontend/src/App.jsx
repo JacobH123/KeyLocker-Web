fd4090b70b9bde8fbafd09ef8fa8ed89ed80b82d
Jacob H <jacobhellebrand@csu.fullerton.edu>
2025-08-05 18:10:40 -0700

    seperated secure and public routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Downloads from './pages/Downloads';
 import Settings from "./pages/Settings";
 import HomePage from "./pages/HomePage";
@@ -7,25 +6,39 @@ import LoginPage from './pages/LoginPage';
 import Signup from './pages/Signup';
 import Updates from './pages/UpdatePage';
 import MainContent from './pages/MainContent';
+import EmailVerify from "./pages/EmailVerify";
+import { AuthProvider, ProtectedRoute } from './components/RouteProtection';
+
 export const App = () => {
-
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen bg-black">
-
-          <main className="flex-1 bg-gradient-to-br from-[#1a1a2e] via-black to-[#1a1a2e] text-white  overflow-auto">
+    <AuthProvider>
+      <Router>
+        <div className="flex flex-col min-h-screen bg-black">
+          <main className="flex-1 bg-gradient-to-br from-[#1a1a2e] via-black to-[#1a1a2e] text-white overflow-auto">
             <Routes>
+              {/* Public routes */}
               <Route path="/" element={<HomePage />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/downloads" element={<Downloads />} />
-              <Route path="/settings" element={<Settings />} />
-              <Route path="/signup" element={<Signup/>} />
-              <Route path="/updates" element={<Updates/>} />
-              <Route path="/passwords" element={<MainContent/>} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/updates" element={<Updates />} />
+              <Route path="/emailverify" element={<EmailVerify />} />
+
+              {/* Protected routes */}
+              <Route path="/passwords" element={
+                <ProtectedRoute>
+                  <MainContent />
+                </ProtectedRoute>
+              } />
+              <Route path="/settings" element={
+                <ProtectedRoute>
+                  <Settings />
+                </ProtectedRoute>
+              } />
             </Routes>
           </main>
-        
-      </div>
-    </Router>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 };
