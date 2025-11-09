e7fac1d8fa2a2b7053f949982a5e938d77ba6ce7
Jacob H <jacobhellebrand@csu.fullerton.edu>
2025-11-09 14:42:07 -0800

    Added verify_link endpoint

--- backend/app/verify.py
+++ backend/app/verify.py
@@ -1,6 +1,6 @@
 from . import db
-from flask import Blueprint, request, jsonify
-
+from flask import Blueprint, request, jsonify,redirect
+import os
 from .models import User
 from datetime import datetime, timedelta
 import secrets
@@ -57,6 +57,34 @@ def verify_token():
         return jsonify({"error": "Unauthorized"}), 401
 
     return jsonify({"user": {"id": user.id, "email": user.email}})
+
+
+FRONTEND_URL = os.getenv("FRONTEND_URL", "http://127.0.0.1:5173")
+@verify_bp.route("/verify/<code>", methods=["GET"])
+def verify_link(code):
+    user = User.query.filter_by(verification_code=code).first()
+
+    if not user:
+        return jsonify({'error': 'Invalid verification link'}), 404
+
+    if not user.code_expires_at or user.code_expires_at < datetime.utcnow():
+        return jsonify({'error': 'Verification link expired'}), 409
+
+    
+    user.verified = True
+    user.verification_code = None
+    user.code_expires_at = None
+
+    
+    temp_token = secrets.token_urlsafe(32)
+    user.temp_token = temp_token
+    user.temp_token_expires_at = datetime.utcnow() + timedelta(minutes=15)
+
+    db.session.commit()
+
+    return redirect(f"{FRONTEND_URL}/createpassword?temp_token={temp_token}&email={user.email}")
+
+
 
 
         
