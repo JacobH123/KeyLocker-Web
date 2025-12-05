0a42b166a3c4e26ecb4efc61bee31d50aad3f790
Jacob H <jacobhellebrand@csu.fullerton.edu>
2025-12-04 20:55:20 -0800

    fixed linting issue

--- frontend/tests/setup.js
+++ frontend/tests/setup.js
@@ -1,10 +1,10 @@
-import { expect, afterEach } from 'vitest';
+import { expect, afterEach,beforeAll  } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import * as matchers from '@testing-library/jest-dom/matchers';
 
 beforeAll(() => {
-  if (typeof global.structuredClone === 'undefined') {
-    global.structuredClone = (obj) => JSON.parse(JSON.stringify(obj));
+  if (typeof globalThis.structuredClone === 'undefined') {
+    globalThis.structuredClone = (obj) => JSON.parse(JSON.stringify(obj));
   }
 });
 
