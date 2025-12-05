b0a98cefe3a3a4e3764a6be895c096cf50314c8d
Jacob H <jacobhellebrand@csu.fullerton.edu>
2025-12-04 20:52:04 -0800

    trying to fix github actions issue

--- frontend/tests/setup.js
+++ frontend/tests/setup.js
@@ -2,6 +2,13 @@ import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import * as matchers from '@testing-library/jest-dom/matchers';
 
+beforeAll(() => {
+  if (typeof global.structuredClone === 'undefined') {
+    global.structuredClone = (obj) => JSON.parse(JSON.stringify(obj));
+  }
+});
+
+
 expect.extend(matchers);
 
 afterEach(() => {
